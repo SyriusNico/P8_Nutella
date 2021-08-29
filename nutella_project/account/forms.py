5b689e4d1e4a241cd0e89d55196f888c31252ae1
Nicolas <pnschlei@gmail.com>
2021-08-29 20:14:12 +0200

    add forms.py

--- nutella_project/account/forms.py
+++ nutella_project/account/forms.py
@@ -1,5 +1,32 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
-class NameForm(forms.Form):
-    user_name = forms.CharField(label='user_name', max_length=100)
-    email = forms.CharField(label='user_name', max_length=100)
+class RegisterForm(UserCreationForm):
+    
+    # Form's field
+    username = forms.CharField(label=('Nom utilisateur'))
+    password1 = forms.CharField(label=("Mot de passe"),
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmer', widget=forms.PasswordInput)
+    email = forms.EmailField()
+
+    # Confirm the password
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Le mot de passe incorect")
+
+        return password2
+
+    # Store the user
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1']
+        )
+        return user
