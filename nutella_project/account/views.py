9dd9653a2322054819f31507d8b9b0b245fd5053
Nicolas <pnschlei@gmail.com>
2021-08-29 20:16:01 +0200

    add register function

--- nutella_project/account/views.py
+++ nutella_project/account/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import RegisterForm
 
 # Create your views here.
-def login(request):
-	context = {}
-	return render(request, 'account/login.html', context)
+def register(request):
+    if request.method == 'POST':
+        registration = RegisterForm(request.POST)
+        if registration.is_valid():
+            registration.save()
+            messages.success(request, 'Inscription réussite')
+            return redirect('myAccount')
+    else:
+        registration = RegisterForm()
+
+    return render(request, 'account/register.html', {'form': registration})
+
+
 
 def myAccount(request):
 	context = {}
