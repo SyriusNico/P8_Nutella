35c64f1729fdeec3592635babf4f2ed38d5bbb60
Nicolas <pnschlei@gmail.com>
2021-09-04 04:13:06 +0200

    first push of OFF API

--- nutella_project/foods/models.py
+++ nutella_project/foods/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Categories(models.Model):
 	name = models.CharField(max_length=70)
 
+	def __str__(self):
+		return self.name
+
 class Products(models.Model):
 	product_name = models.CharField(max_length=255)
 	code = models.IntegerField()
@@ -12,6 +15,13 @@ class Products(models.Model):
 	url = models.URLField(max_length=255)
 	category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
 
-# class Substitutes(models.Model):
-# 	product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
-# 	substitue_id = models.ForeignKey(Products, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.product_name
+
+class Substitutes(models.Model):
+	choosen_product = models.ForeignKey(Products, on_delete=models.CASCADE,
+											related_name='choosen_product')
+	substitue = models.ForeignKey(Products, on_delete=models.CASCADE,
+											 related_name='substitute')
+	def __str__(self):
+		return self.substitue
--- nutella_project/foods/views.py
+++ nutella_project/foods/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .offApi import OffApi
+import requests
+
 
 # Create your views here.
 def result(request):
-	context = {}
-	return render(request, 'foods/result.html', context)
+	api = OffApi()
+	responses = requests.get(api.url, api.payload)
+	offData = responses.json()
+
+	context = {
+		'product_name' : offData['product_name'],
+		'code' : offData['code'],
+		'nutrition_grade' : offData['nutrition_grade_fr'],
+		'stores' : offData['stores'],
+		'url' : offData['url']
+	}
+	render
+	
