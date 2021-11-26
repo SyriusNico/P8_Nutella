492645b29a813b80342154aa1e89c27b82c74cf4
Nicolas <pnschlei@gmail.com>
2021-11-26 02:18:09 +0100

    install django-heroku

--- nutella_project/nutella_project/settings.py
+++ nutella_project/nutella_project/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,3 +147,6 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
