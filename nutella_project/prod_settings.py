67dd70fd3ab65450a920f0eae48ce5967a5eaaf6
Nicolas <pnschlei@gmail.com>
2021-11-27 22:35:00 +0100

    organize folders

---
+++ nutella_project/prod_settings.py
@@ -0,0 +1,7 @@
+from nutella_project.settings import DEBUG, SECRET_KEY, ALLOWED_HOSTS
+
+DEBUG = False
+
+SECRET_KEY = 's+a=38t+knz2p!bx#(@mm$9(x670qo#z%iuld%*6yg8rk34_wh'
+
+ALLOWED_HOSTS = ['purbeurre-app.herokuapp.com']
--- nutella_project/settings.py
+++ nutella_project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8z1t2)awwqen=bx==l2doak57wkou5%#81%565wbftgrn6$j$j'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
